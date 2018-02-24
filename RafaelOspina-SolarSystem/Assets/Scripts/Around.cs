581db4bceb030def324abed7d7666c27a583b41f
Rafael Ospina <rafaelospiname@gmail.com>
2018-02-24 13:32:19 -0500

    Trabajo Sistema Solar - Faltan Anillos

---
+++ RafaelOspina-SolarSystem/Assets/Scripts/Around.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Around : MonoBehaviour {
+
+	public float translationSpeed;
+	//public float fixedDistance;
+	public Transform sunPivot;
+
+	/*void Update () {
+		this.transform.position = (transform.position - sunPivot.transform.position).normalized * fixedDistance + sunPivot.transform.position;
+	}*/
+
+	void LateUpdate() {
+		this.transform.RotateAround (sunPivot.position, Vector3.up, -translationSpeed);
+	}
+}
